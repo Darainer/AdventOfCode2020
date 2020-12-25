56ee32c3ec88f8b712dc61cc830943b55ec4ea6e
Darainer <ryanjmatthews@gmail.com>
2020-12-25 20:50:48 +0100

    refactor to function for part 2

--- Day1_expense_report.py
+++ Day1_expense_report.py
@@ -1,30 +1,32 @@
 input_file = "Day1input.txt"
-#List_of_expenses = []
 set_of_expenses = set()
 
 with open(input_file, 'r') as file:
     for lines in file:
         l = lines.strip('\n')
-        #List_of_expenses.append(int(l))
         set_of_expenses.add(int(l))
 
-#List_of_expenses.sort()
+def find_matching_two_numbers(Sum_value :int, set_of_expenses: set) -> [int,int]:
+    for expense in set_of_expenses:
+        matching_remaining_expense = Sum_value - expense
+        if matching_remaining_expense in set_of_expenses:
+            answer = [expense, matching_remaining_expense]
+            return answer   
 
-#print("sorted list", List_of_expenses,  sep='\n')
+
+#
+
+#part 1: find the two numbers that add up to 2020 
+# go through set
+# for each member check if there exists a key which adds to 2020
 Sum_value = 2020
-#find the two numbers that add up to 2020 
+answer = find_matching_two_numbers(2020,set_of_expenses)
+print("part 1 answer = " , answer[0]*answer[1])
 
-# go through list from front
-#find matching_second_expense
-# search from back of second list
+#part 2
+# go through set
+# for each member check if there exists a triplet where (member + sum(te) which adds to 2020
 
 
 
-for expense in set_of_expenses:
-    matching_second_expense = Sum_value - expense
-    if matching_second_expense in set_of_expenses:
-        answer = expense * matching_second_expense
-        break
-
-print(answer)
 

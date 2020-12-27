6a76e215b89eaa06a6b1c992a729e38d954a6db8
Darainer <ryanjmatthews@gmail.com>
2020-12-27 20:28:34 +0100

    passing day 2

--- Day1_expense_report.py
+++ Day1_expense_report.py
@@ -12,10 +12,17 @@ def find_matching_two_numbers(Sum_value :int, set_of_expenses: set) -> [int,int]
         if matching_remaining_expense in set_of_expenses:
             answer = [expense, matching_remaining_expense]
             return answer   
+    return [0,0]
 
-
-#
-
+def find_matching_three_numbers(Sum_value :int, set_of_expenses: set) -> [int,int,int]:
+    for expense1 in set_of_expenses:
+        matching_remaining_expense = Sum_value - expense1
+        for expense2 in set_of_expenses:
+            if expense1 != expense2:
+                answer = find_matching_two_numbers(matching_remaining_expense,set_of_expenses)
+                if answer[0] != 0:
+                    return [expense1, answer[0], answer[1]]
+  
 #part 1: find the two numbers that add up to 2020 
 # go through set
 # for each member check if there exists a key which adds to 2020
@@ -26,7 +33,9 @@ print("part 1 answer = " , answer[0]*answer[1])
 #part 2
 # go through set
 # for each member check if there exists a triplet where (member + sum(te) which adds to 2020
-
-
+sum_value = 2020
+answer2 = find_matching_three_numbers(2020, set_of_expenses)
+print("numbers that add to 2020", answer2[0], answer2[1], answer2[2])
+print( "part 2 answer = ", answer2[0]*answer2[1]*answer2[2])
 
 

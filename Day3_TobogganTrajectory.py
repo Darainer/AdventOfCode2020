8d14f84e7216132fbe49871617207514bf3f5e83
Darainer <ryanjmatthews@gmail.com>
2020-12-29 19:58:29 +0100

    fixed line reading bug: day3 part1 passing

--- Day3_TobogganTrajectory.py
+++ Day3_TobogganTrajectory.py
@@ -1,17 +1,15 @@
 input_file = 'Day3input.txt'
-
-
 right_increment = 3
+line_length = 31
 row_position = 0
 number_of_trees = 0 
+row_count = 0
 
 with open(input_file, 'r') as file:
-    line = file.readline().strip('\n')
-    line_length = len(line)
     for lines in file:
         l = lines.strip('\n')
         if(l[row_position] == '#'):
             number_of_trees+=1
-        row_position = (row_position + 3) % line_length    
-        
-print(number_of_trees)        
+        row_position = (row_position + 3) % (line_length) 
+           
+print(number_of_trees)

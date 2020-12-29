3377df7713d55663ca26b22284fbf3669bdcbcb4
Darainer <ryanjmatthews@gmail.com>
2020-12-29 10:08:02 +0100

    implemented modulo wrap for duplicating trees

--- Day3_TobogganTrajectory.py
+++ Day3_TobogganTrajectory.py
@@ -6,13 +6,12 @@ row_position = 0
 number_of_trees = 0 
 
 with open(input_file, 'r') as file:
+    line = file.readline().strip('\n')
+    line_length = len(line)
     for lines in file:
         l = lines.strip('\n')
-        if row_position > len(l)-1:
-            break
         if(l[row_position] == '#'):
             number_of_trees+=1
-        row_position+=right_increment
+        row_position = (row_position + 3) % line_length    
         
-
 print(number_of_trees)        

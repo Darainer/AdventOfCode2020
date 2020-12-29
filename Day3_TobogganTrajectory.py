30d44ad31bd37bddc5fc69890889103ddb0e2ca7
Darainer <ryanjmatthews@gmail.com>
2020-12-29 22:03:49 +0100

    calculated all slopes

--- Day3_TobogganTrajectory.py
+++ Day3_TobogganTrajectory.py
@@ -13,9 +13,18 @@ def calculate_trees_hit( input_file, right_increment: int, vertical_increment: i
                 l = lines.strip('\n')
                 if(l[row_position] == '#'):
                     number_of_trees+=1
-                row_position = (row_position + 3) % (line_length) 
+                row_position = (row_position + right_increment) % (line_length) 
     return number_of_trees      
 
 #Part 1
 Trees_right3_down1 = calculate_trees_hit(input_file,3, 1)
 print(Trees_right3_down1)
+
+#part2
+Trees_right1_down1 = calculate_trees_hit(input_file,1, 1)
+Trees_right5_down1 = calculate_trees_hit(input_file,5, 1)
+Trees_right7_down1 = calculate_trees_hit(input_file,7, 1)
+Trees_right1_down2 = calculate_trees_hit(input_file,1, 2)
+
+answer = Trees_right1_down1*Trees_right3_down1*Trees_right5_down1*Trees_right7_down1*Trees_right1_down2
+print(answer)

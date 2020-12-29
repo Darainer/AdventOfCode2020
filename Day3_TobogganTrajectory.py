7ee56fe95ab4a36c9763441d4b10f3e0d2cff92a
Darainer <ryanjmatthews@gmail.com>
2020-12-29 21:45:09 +0100

    refactor to function for partb

--- Day3_TobogganTrajectory.py
+++ Day3_TobogganTrajectory.py
@@ -1,15 +1,21 @@
 input_file = 'Day3input.txt'
-right_increment = 3
 line_length = 31
-row_position = 0
-number_of_trees = 0 
-row_count = 0
 
-with open(input_file, 'r') as file:
-    for lines in file:
-        l = lines.strip('\n')
-        if(l[row_position] == '#'):
-            number_of_trees+=1
-        row_position = (row_position + 3) % (line_length) 
-           
-print(number_of_trees)
+
+def calculate_trees_hit( input_file, right_increment: int, vertical_increment: int ) -> int:
+    row_position = 0
+    number_of_trees = 0 
+    row_count = 0
+    with open(input_file, 'r') as file:
+        for lines in file:
+            row_count+=1
+            if (row_count %vertical_increment == 0):
+                l = lines.strip('\n')
+                if(l[row_position] == '#'):
+                    number_of_trees+=1
+                row_position = (row_position + 3) % (line_length) 
+    return number_of_trees      
+
+#Part 1
+Trees_right3_down1 = calculate_trees_hit(input_file,3, 1)
+print(Trees_right3_down1)

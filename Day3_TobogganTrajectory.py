0bb1956fc4009dc43ccd0c88e4500bcbdce21954
Darainer <ryanjmatthews@gmail.com>
2020-12-31 13:48:15 +0100

    Day3 part 2: modulo cmp fix to skip correct lines

--- Day3_TobogganTrajectory.py
+++ Day3_TobogganTrajectory.py
@@ -5,15 +5,16 @@ line_length = 31
 def calculate_trees_hit( input_file, right_increment: int, vertical_increment: int ) -> int:
     row_position = 0
     number_of_trees = 0 
-    row_count = 0
+    row_count = 1
     with open(input_file, 'r') as file:
         for lines in file:
             row_count+=1
-            if (row_count %vertical_increment == 0):
+            if (row_count % vertical_increment != 1):
                 l = lines.strip('\n')
                 if(l[row_position] == '#'):
                     number_of_trees+=1
                 row_position = (row_position + right_increment) % (line_length) 
+
     return number_of_trees      
 
 #Part 1

b636aff95d34ab862ce4989241adc6d7b06f1c9e
Darainer <ryanjmatthews@gmail.com>
2021-01-01 10:49:22 +0100

    refactor to remove duplication

--- Day3_TobogganTrajectory.py
+++ Day3_TobogganTrajectory.py
@@ -1,7 +1,6 @@
 input_file = 'Day3input.txt'
 line_length = 31
 
-
 def calculate_trees_hit( input_file, right_increment: int, vertical_increment: int ) -> int:
     row_position = 0
     number_of_trees = 0 
@@ -14,18 +13,15 @@ def calculate_trees_hit( input_file, right_increment: int, vertical_increment: i
                 if(l[row_position] == '#'):
                     number_of_trees+=1
                 row_position = (row_position + right_increment) % (line_length) 
-
     return number_of_trees      
 
 #Part 1
 Trees_right3_down1 = calculate_trees_hit(input_file,3, 1)
-print(Trees_right3_down1)
+print("part 1 answer:",Trees_right3_down1)
 
 #part2
-Trees_right1_down1 = calculate_trees_hit(input_file,1, 1)
-Trees_right5_down1 = calculate_trees_hit(input_file,5, 1)
-Trees_right7_down1 = calculate_trees_hit(input_file,7, 1)
-Trees_right1_down2 = calculate_trees_hit(input_file,1, 2)
-
-answer = Trees_right1_down1*Trees_right3_down1*Trees_right5_down1*Trees_right7_down1*Trees_right1_down2
-print(answer)
+slopes = [[3,1],[1,1], [5,1], [7,1], [1,2]]
+part2_answer = 1
+for slope_pair in slopes:
+    part2_answer *= calculate_trees_hit(input_file,slope_pair[0], slope_pair[1])
+print("part 2 answer:", part2_answer)

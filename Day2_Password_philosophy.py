716d1c414fe90f65f79cef59b6b642d3ea92b138
Darainer <ryanjmatthews@gmail.com>
2021-02-07 11:06:25 +0100

    pyflakes fixes

--- Day2_Password_philosophy.py
+++ Day2_Password_philosophy.py
@@ -4,32 +4,36 @@ input_file = "Day2input.txt"
 # min-max char: string
 # 2-4 p: vpkpp
 
-#todo: im missing using something like a cpp struct data structure here
+# todo: im missing using something like a cpp struct data structure here
 
-def parsePasswordRule(line: str) -> [int , int , str]:
+
+def parsePasswordRule(line: str) -> [int, int, str]:
     [minmax, char_raw, string] = line.split()
     [minimum, maximum] = minmax.split("-")
     char = char_raw.split(":")
-    return [int(minimum),int(maximum),char[0],string]
+    return [int(minimum), int(maximum), char[0], string]
 
-def checkPWcompliance_part1(PWRule : list) -> bool:
+
+def checkPWcompliance_part1(PWRule: list) -> bool:
     numberchars_inPW = PWRule[3].count(PWRule[2])
     if PWRule[0] <= numberchars_inPW <= PWRule[1]:
         return True
     else:
         return False
 
-def checkPWcompliance_part2(PWRule : list) -> bool:
-    #xor first or second index of the string matches char
-    if (PWRule[3][PWRule[0]-1] == PWRule[2]) ^ (PWRule[3][PWRule[1]-1] == PWRule[2]):  
+
+def checkPWcompliance_part2(PWRule: list) -> bool:
+    # xor first or second index of the string matches char
+    if (PWRule[3][PWRule[0]-1] == PWRule[2]) ^ (PWRule[3][PWRule[1]-1] == PWRule[2]): 
         return True
     return False
 
-CompliantPWs_part1 = 0 #count
-CompliantPWs_part2 = 0 #count
+
+CompliantPWs_part1 = 0  # count
+CompliantPWs_part2 = 0  # count
 with open(input_file, 'r') as file:
     for lines in file:
-        PasswordRule = parsePasswordRule(lines) 
+        PasswordRule = parsePasswordRule(lines)
         CompliantPWs_part1 += checkPWcompliance_part1(PasswordRule)
         CompliantPWs_part2 += checkPWcompliance_part2(PasswordRule)
 

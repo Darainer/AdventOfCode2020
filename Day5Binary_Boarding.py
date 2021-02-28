5b9e2af4c08c4fde87a2b22b0c4f2958572e4982
Darainer <ryanjmatthews@gmail.com>
2021-02-28 17:53:54 +0100

    refactor out the boarding pass parser

--- Day5Binary_Boarding.py
+++ Day5Binary_Boarding.py
@@ -1,21 +1,23 @@
 import numpy as np
 input_file = 'Day5input.txt'
 
-def ParseBoardingPass(raw_row: str,raw_col: str) -> [int, int]:
-    bin_row = raw_row.replace("F","0").replace("B","1")
-    row = int(bin_row,2)
-    bin_col = raw_col.replace("L","0").replace("R","1")
-    col = int(bin_col,2)
-    return [row,col]
 
-all_seats = np.zeros((128,8))
+def ParseBoardingPass(raw_row: str, raw_col: str) -> [int, int]:
+    bin_row = raw_row.replace("F", "0").replace("B", "1")
+    row = int(bin_row, 2)
+    bin_col = raw_col.replace("L", "0").replace("R", "1")
+    col = int(bin_col, 2)
+    return [row, col]
+
+
+all_seats = np.zeros((128, 8))
 with open(input_file, 'r') as file:
-    highestSeatID=0
+    highestSeatID = 0
     for line in file:
-        [row,column] = ParseBoardingPass(line[0:7],line[7:10]) #second slicing element is exclusive
+        [row, column] = ParseBoardingPass(line[0:7], line[7:10])  # second slicing element is exclusive
         seatID = row*8 + column
-        all_seats[row,column] = 1
-        highestSeatID = max(highestSeatID,seatID)
+        all_seats[row, column] = 1
+        highestSeatID = max(highestSeatID, seatID)
 
-print(highestSeatID) #part1 
+print(highestSeatID)  # part1
 print(all_seats)  # part2: seat Map of plane

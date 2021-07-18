46544739ad81f0c72038a0413cb4329268ee486f
Darainer <ryanjmatthews@gmail.com>
2021-07-18 13:57:07 +0200

    passing day6 part2

--- Day6_Custom_customs.py
+++ Day6_Custom_customs.py
@@ -14,10 +14,19 @@ def Customs_checker(text: TextIOWrapper) -> bool:
             count += len(group)
             group.clear()
     count += len(group)
-    group.clear()
     return count
 
 
-input_file = 'Day6_input.txt'
-with open(input_file, 'r') as file:
-    print(Customs_checker(file))
+def Count_group_common_answers(text: TextIOWrapper) -> bool:
+    count = 0
+    group = list()
+    for line in text:
+        if line != '\n':
+            group.append(set())
+            raw_line = line.strip('\n')
+            for string in raw_line:
+                group[-1].add(string)
+        else:
+            count += len(group.pop().intersection(*group))
+            group.clear()
+    return count
--- test_Day6.py
+++ test_Day6.py
@@ -1,4 +1,4 @@
-from Day6_Custom_customs import Customs_checker
+from Day6_Custom_customs import Customs_checker, Count_group_common_answers
 
 
 def test_Day6_exampleInputTest():
@@ -14,3 +14,17 @@ def test_Day6_part1InputTest():
         output_sum = Customs_checker(file)
     print('day6 part a output = ', output_sum)
     assert output_sum == 6782
+
+
+def test_Day6part2_exampleInputTest():
+    input_file = "Day6_simpletest.txt"
+    with open(input_file, 'r') as file:
+        output_sum = Count_group_common_answers(file)
+    assert output_sum == 6
+
+
+def test_Day6part2_InputTest():
+    input_file = "Day6_input.txt"
+    with open(input_file, 'r') as file:
+        output_sum = Count_group_common_answers(file)
+    assert output_sum == 3596

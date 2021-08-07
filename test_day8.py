4f19ceed48a86d24346f74c35008b77e636a0d78
Darainer <ryanjmatthews@gmail.com>
2021-08-07 19:53:49 +0200

    update day8 test

--- test_day8.py
+++ test_day8.py
@@ -1,7 +1,6 @@
-from Day8_HandheldHalting import parse_instructions, run_part1, modify_boot_code
+from Day8_HandheldHalting import parse_instructions, run_part1, part2_modify_boot_code
 
 test_parse_result = [["nop", 0], ["acc", 1], ["jmp", 4], ["acc", 3], ["jmp", -3], ["acc", -99], ["acc", 1], ["jmp", -4], ["acc", 6]]
-
 
 def test_parser_part1():
     input_file = "Day8_simple_input.txt"
@@ -17,5 +16,5 @@ def test_part1():
 
 def test_part2():
     input_file = "Day8_input.txt"
-    [error_code, accumulator_value] = modify_boot_code(input_file)
+    [error_code, accumulator_value] = part2_modify_boot_code(input_file)
     assert [error_code, accumulator_value] == [0, 1016]
